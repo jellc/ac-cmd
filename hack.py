5e8c9a851ca6e731f0193a5ce349de215a40466c
jell <euler31415@icloud.com>
2021-04-09 23:39:54 +0900

    ren

--- hack.py
+++ hack.py
@@ -19,31 +19,39 @@ def main(args):
     if len(args) == 1:
         logger.info(" Assert debugging...")
         src1 = args[0]
-        tmp1 = './tmp1.exe'
+        exe1 = './exe1.exe'
 
-        subprocess.call(["g++", src1, "-o", tmp1] + tester.options)
+        subprocess.call(["g++", src1, "-o", exe1] + tester.options)
 
         # TODO
-        # if subprocess.call(["oj", "g/i", "--width=2", "-c", tmp1, "--hack", tmp1, gencmd]):
+        # Compare with itself
+        # if subprocess.call(["oj", "g/i", "--width=2", "-c", exe1, "--hack", exe1, gencmd]):
         #     exit(1)
 
         logger.critical(pycolor.BRIGHT_RED + " " + src1 + " is hacked!")
 
-        os.remove(tmp1)
+        os.remove(exe1)
 
     else:
         src1 = args[0]
         src2 = args[1]
-        tmp1 = './tmp1.exe'
-        tmp2 = './tmp2.exe'
+        exe1 = './exe1.exe'
+        exe2 = './exe2.exe'
 
-        subprocess.call(["g++", src1, "-o", tmp1] + tester.options)
-        subprocess.call(["g++", src2, "-o", tmp2] + tester.options)
+        logger.info(' Compiling {} ...'.format(src1))
+        if subprocess.call(["g++", src1, "-o", exe1] + tester.options):
+            logger.error(' Compilation error: {}', src1)
+            exit(1)
 
-        if subprocess.call(["oj", "g/i", "--width=2", "-c", tmp2, "--hack", tmp1, gencmd]):
+        logger.info(' Compiling {} ...'.format(src2))
+        if subprocess.call(["g++", src2, "-o", exe2] + tester.options):
+            logger.error(' Compilation error: {}', src2)
+            exit(1)
+
+        if subprocess.call(["oj", "g/i", "--width=2", "-c", exe2, "--hack", exe1, gencmd]):
             exit(1)
 
         logger.critical(pycolor.BRIGHT_RED + " " + src1 + " hacked by " + src2 + " !")
 
-        os.remove(tmp1)
-        os.remove(tmp2)
+        os.remove(exe1)
+        os.remove(exe2)
